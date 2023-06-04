690c0a54adc0b0e719f3687d1a8dc36c725d40e8
Jimmyyy <chenjimmy91@hotmail.com>
2023-06-04 23:12:31 +0800

    注册照片上传bug fix

--- lib/infoinitial.dart
+++ lib/infoinitial.dart
@@ -6,7 +6,6 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'settings_page.dart';
-import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class InfoInitialPage extends StatefulWidget {
   const InfoInitialPage({Key? key}) : super(key: key);
@@ -22,10 +21,10 @@ class _InfoInitialPageState extends State<InfoInitialPage> {
   TextEditingController aboutController = TextEditingController();
 
   String? _userName;
-  String _imageUrl =
-      'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80';
+  // String _imageUrl =
+  //     'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80';
   String? _about;
-  late File _image;
+  File? _image;
 
   Future<void> _getImage() async {
     final ImagePicker _picker = ImagePicker();
@@ -33,10 +32,9 @@ class _InfoInitialPageState extends State<InfoInitialPage> {
 
     if (image != null) {
       setState(() {
-        _imageUrl = image.path;
-        _image = File(_imageUrl);
-        print(_imageUrl);
-        print(_image);
+        // _imageUrl = image.path;
+        // _image = File(_imageUrl);
+        _image = File(image.path);
       });
     }
   }
@@ -84,36 +82,41 @@ class _InfoInitialPageState extends State<InfoInitialPage> {
               const SizedBox(
                 height: 15,
               ),
-              GestureDetector(
-                onTap: () {
-                  // 处理点击事件的代码
-                  print('Container clicked!');
-                  _getImage();
-                },
-                child: Container(
-                  width: 130,
-                  height: 130,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 4,
-                      color: Theme.of(context).scaffoldBackgroundColor,
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        spreadRadius: 2,
-                        blurRadius: 10,
-                        color: Colors.black.withOpacity(0.1),
-                        offset: const Offset(0, 10),
+              _image == null
+                  ? TextButton(
+                      onPressed: _getImage,
+                      child: Text("选择头像图片"),
+                    )
+                  : GestureDetector(
+                      onTap: () {
+                        // 处理点击事件的代码
+                        _getImage();
+                      },
+                      child: Container(
+                        width: 130,
+                        height: 130,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 4,
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1),
+                              offset: const Offset(0, 10),
+                            ),
+                          ],
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.contain,
+                            // image: NetworkImage(_imageUrl),
+                            image: FileImage(_image!),
+                          ),
+                        ),
                       ),
-                    ],
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.contain,
-                      image: NetworkImage(_imageUrl),
                     ),
-                  ),
-                ),
-              ),
               const SizedBox(
                 height: 35,
               ),
@@ -165,22 +168,46 @@ class _InfoInitialPageState extends State<InfoInitialPage> {
                           FirebaseAuth.instance.currentUser?.uid;
                       String? token =
                           await FirebaseMessaging.instance.getToken();
-                      firebase_storage.Reference storageRef = firebase_storage
-                          .FirebaseStorage.instance
-                          .ref()
-                          .child('profiles')
-                          .child(uid!);
-                      await storageRef.putFile(File(_imageUrl));
-                      UploadTask uploadTask =
-                          storageRef.putFile(File(_imageUrl));
-                      TaskSnapshot snap = await uploadTask;
-                      String downloadurl = await snap.ref.getDownloadURL();
+                      // firebase_storage.Reference storageRef = firebase_storage
+                      //     .FirebaseStorage.instance
+                      //     .ref()
+                      //     .child('profiles')
+                      //     .child(uid!);
+                      // await storageRef.putFile(File(_imageUrl));
+                      // UploadTask uploadTask =
+                      //     storageRef.putFile(File(_imageUrl));
+                      // TaskSnapshot snap = await uploadTask;
+                      // String downloadurl = await snap.ref.getDownloadURL();
+
+                      // upload profile image
+                      if (_image == null) {
+                        // TODO: show somthing on the screen
+                        return;
+                      }
+                      Reference referenceRoot = FirebaseStorage.instance.ref();
+                      Reference referenceDirPosts =
+                          referenceRoot.child('posts');
+                      String uniqueFileName =
+                          DateTime.now().millisecondsSinceEpoch.toString();
+
+                      Reference referenceToUpload =
+                          referenceDirPosts.child(uniqueFileName);
+
+                      String? profileImageUrl;
+                      try {
+                        await referenceToUpload.putFile(_image!);
+                        profileImageUrl =
+                            await referenceToUpload.getDownloadURL();
+                      } catch (e) {
+                        print(e);
+                      }
+
                       FirebaseFirestore.instance // TODO: change Uses model
                           .collection('users')
                           .doc(uid)
                           .set({
                         'name': _userName, //,yes I know.
-                        'profile': downloadurl,
+                        'profile': profileImageUrl,
                         // 'profile': _imageUrl,
                         'uid': uid,
                         'introduction': _about,
