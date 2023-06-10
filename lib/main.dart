4b27fdc2114babdd86f1b8305c33a68d08641d03
JC <101612634+Jc-Bravo@users.noreply.github.com>
2023-06-11 02:22:22 +0800

    功能实现:添加了动画, 改变了字体和色彩

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'app_state.dart';
 import 'firebase_options.dart';
@@ -13,133 +14,172 @@ import 'all_users.dart';
 import 'infoinitial.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FirebaseUIAuth.configureProviders([
-    EmailAuthProvider(),
-  ]);
-  runApp(ChangeNotifierProvider(
-    create: (context) => ApplicationState(),
-    builder: ((context, child) => const MyApp()),
-  ));
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+options: DefaultFirebaseOptions.currentPlatform,
+);
+FirebaseUIAuth.configureProviders([
+EmailAuthProvider(),
+]);
+runApp(ChangeNotifierProvider(
+create: (context) => ApplicationState(),
+builder: ((context, child) => const MyApp()),
+));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const MyApp({super.key});
 
-  static const String _title = 'Flutter Code Sample';
+static const String _title = 'Flutter Code Sample';
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+title: _title,
+theme: ThemeData(
+primarySwatch: Colors.purple,
+textTheme: GoogleFonts.latoTextTheme(
+Theme.of(context).textTheme,
+),
+),
+home: MyStatefulWidget(),
+);
+}
+
 }
 
 class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({super.key});
+const MyStatefulWidget({super.key});
 
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+@override
+State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static final List<Widget> _widgetOptions = <Widget>[
-    const HomePage(),
-    const FavoritePage(),
-    Consumer<ApplicationState>(
-        builder: (context, appState, _) => FollowingPage(
-              follows: appState.follows,
-              followingUsers: appState.followingUsers,
-            )),
-    Consumer<ApplicationState>(
-        builder: (context, appState, _) => UsersPage(
-              follows: appState.follows,
-            )),
-    Consumer<ApplicationState>(
-        builder: (context, appState, _) => AccountPage(
-              loggedIn: appState.loggedIn,
-              localUser: appState.localUser,
-            )),
-  ];
+class _MyStatefulWidgetState extends State<MyStatefulWidget> with SingleTickerProviderStateMixin {
+int _selectedIndex = 0;
+late AnimationController _controller;
+late Animation<double> _animation;
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+static final List<Widget> _widgetOptions = <Widget>[
+const HomePage(),
+const FavoritePage(),
+Consumer<ApplicationState>(
+builder: (context, appState, _) => FollowingPage(
+follows: appState.follows,
+followingUsers: appState.followingUsers,
+)),
+Consumer<ApplicationState>(
+builder: (context, appState, _) => UsersPage(
+follows: appState.follows,
+)),
+Consumer<ApplicationState>(
+builder: (context, appState, _) => AccountPage(
+loggedIn: appState.loggedIn,
+localUser: appState.localUser,
+)),
+];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Consumer<ApplicationState>(builder: (context, appState, _) {
-        if (appState.loggedIn) {
-          return Center(
-            child: _widgetOptions.elementAt(_selectedIndex),
-          );
-        }
-        return SignInScreen(
-          actions: [
-            AuthStateChangeAction((context, state) {
-              if (state is SignedIn || state is UserCreated) {
-                var user = (state is SignedIn)
-                    ? state.user
-                    : (state as UserCreated).credential.user;
-                if (user == null) {
-                  return;
-                }
+@override
+void initState() {
+super.initState();
+_controller = AnimationController(
+duration: const Duration(milliseconds: 300),
+vsync: this,
+);
+_animation = Tween<double>(begin: 0, end: 2 * 3.14).animate(_controller);
+}
 
-                if (state is UserCreated) {
-                  //跳转到profile创建页面
-                  user.updateDisplayName(user.email!.split('@')[0]);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const InfoInitialPage()),
-                  );
-                }
-              }
-            })
-          ],
-        );
-      }),
+void _onItemTapped(int index) {
+_controller.forward().then((value) => _controller.reset());
+setState(() {
+_selectedIndex = index;
+});
+}
 
-      // body: Center(
-      //   child: _widgetOptions.elementAt(_selectedIndex),
-      // ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.star),
-            label: 'Favorites',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.people_outline_sharp),
-            label: 'Following',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'Chat',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Account',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.black45,
-        onTap: _onItemTapped,
-      ),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+body: Consumer<ApplicationState>(builder: (context, appState, _) {
+if (appState.loggedIn) {
+return Center(
+child: _widgetOptions.elementAt(_selectedIndex),
+);
+}
+return SignInScreen(
+actions: [
+AuthStateChangeAction((context, state) {
+if (state is SignedIn || state is UserCreated) {
+var user = (state is SignedIn)
+? state.user
+    : (state as UserCreated).credential.user;
+if (user == null) {
+return;
+}
+
+if (state is UserCreated) {
+//跳转到profile创建页面
+user.updateDisplayName(user.email!.split('@')[0]);
+Navigator.push(
+context,
+MaterialPageRoute(
+builder: (context) => const InfoInitialPage()),
+);
+}
+}
+})
+],
+);
+}),
+
+bottomNavigationBar: BottomNavigationBar(
+items: <BottomNavigationBarItem>[
+BottomNavigationBarItem(
+icon: RotationTransition(
+turns: _animation,
+child: Icon(Icons.home),
+),
+label: 'Home',
+),
+BottomNavigationBarItem(
+icon: RotationTransition(
+turns: _animation,
+child: Icon(Icons.star),
+),
+label: 'Favorites',
+),
+BottomNavigationBarItem(
+icon: RotationTransition(
+turns: _animation,
+child: Icon(Icons.people_outline_sharp),
+),
+label: 'Following',
+),
+BottomNavigationBarItem(
+icon: RotationTransition(
+turns: _animation,
+child: Icon(Icons.chat),
+),
+label: 'Chat',
+),
+BottomNavigationBarItem(
+icon: RotationTransition(
+turns: _animation,
+child: Icon(Icons.account_circle),
+),
+label: 'Account',
+),
+],
+currentIndex: _selectedIndex,
+selectedItemColor: Colors.purple,
+unselectedItemColor: Colors.black45,
+onTap: _onItemTapped,
+),
+);
+}
+
+@override
+void dispose() {
+_controller.dispose();
+super.dispose();
+}
 }
